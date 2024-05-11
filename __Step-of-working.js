47ca9d7d951c1cf5a743d2caafdd9a4211042961
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-05-11 08:28:44 +0600

    Read the features data okey

---
+++ __Step-of-working.js
@@ -0,0 +1,25 @@
+// Steps of implementing work successfully
+
+// Step 1: Initial Setup
+/**
+ * Set up your development environment.
+ * Create a new project folder/repository.
+ * Initialize your frontend and backend environments (e.g., React for frontend, Node.js/Express for backend).
+ */
+
+// Step 2: Backend Development
+/**
+ * Set up the backend server using Node.js and Express.
+ * Define API routes for creating, fetching, updating, and deleting assignments. 
+ * Implement authentication endpoints for login and registration.
+ * Set up database models/schema for assignments, users, and authentication.
+ * Implement logic for assignment CRUD operations, including access control.
+ * Implement JWT authentication and authorization for private routes.
+ */
+
+// --------------------------------------------------
+
+// Step 3: Additional Features (Optional)
+/**
+ * Explore and implement Tanstack query mutations in your API.
+ */
--- index.js
+++ index.js
@@ -25,6 +25,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    // Mongodb data collections
+    const featuresCollection = client.db('assignmentGeniusDB').collection('features');
+
+    // Read features from the database
+    app.get('/features', async(req, res) => {
+      const result = await featuresCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
